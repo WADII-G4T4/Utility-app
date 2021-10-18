f5178dced0d992a34cd2e57408b0bf90e8e139f3
Nikita P <niki122001@hotmail.com>
2021-10-18 14:09:39 +0800

    basic draft of bills page

--- frontend/src/pages/Bills.vue
+++ frontend/src/pages/Bills.vue
@@ -1,15 +1,70 @@
 <template>
-    <div>
-
+    <div class="row">
+      <div class="col-12">
+        <card :title="table1.title">
+          <div class="table">
+            <base-table :data="table1.data"
+                        :columns="table1.columns"
+                        thead-classes="text-primary">
+            </base-table>
+          </div>
+        </card>
+      </div>
     </div>
 </template>
 
 <script>
+import { BaseTable } from "@/components";
+const tableColumns = ["Id", "Amount", "Month", "Status"];
+const tableData = [
+    {
+    id: 4,
+    amount: "$103",
+    month: "September",
+    status: "NOT PAID",
+    
+  },
+  {
+    id: 3,
+    amount: "$154",
+    month: "August",
+    status: "PAID",
+  },
+  {
+    id: 2,
+    amount: "$107",
+    month: "July",
+    status: "PAID",
+  },
+  {
+    id: 1,
+    amount: "$109",
+    month: "June",
+    status: "PAID",
+  }
+  ];
+
+
     export default {
-        
-    }
+        components: {
+            BaseTable
+        }, 
+    data() {
+    return {
+      table1: {
+        title: "Bills",
+        columns: [...tableColumns],
+        data: [...tableData]
+      }
+    };
+  }
+    }; 
+
+
+    
 </script>
 
 <style lang="scss" scoped>
+
 
 </style>
--- frontend/src/pages/Community2.vue
+++ frontend/src/pages/Community2.vue
@@ -12,7 +12,8 @@
       </div>
       <div class="container h">
       <button type="button" class="btn-outline-primary mr-1">1</button>
-      <button type="button" onclick = "/Community2.vue" class="btn-outline-primary mr-1">2</button>
+      <button type="button" class="btn-outline-primary mr-1">2</button>
+      <button type="button" class="btn-outline-primary mr-1">3</button>
       <button type="button" class="btn-outline-primary mr-1">></button>
       </div>
     </div>
@@ -24,26 +25,26 @@ const tableColumns = ["Thread", "Username", "Replies"];
 const tableData = [
     {
     id: 1,
-    thread: "why is my gas bill so high?",
+    thread: "how do I change providers?",
     username: "kendricklamar",
     replies: "7",
     
   },
   {
     id: 2,
-     thread: "which appliances to use less to save water?",
+     thread: "saving water",
     username: "angela",
     replies: "15",
   },
   {
     id: 3,
-    thread: "how do I pay my bills on this application?",
+    thread: "energy",
     username: "carlbruner",
     replies: "3",
   },
   {
     id: 4,
-    thread: "is the app accurate? ",
+    thread: "is electricity used up if I leave my socket on?",
     username: "david",
     replies: "9",
   }
